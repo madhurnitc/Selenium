18eb6bd7d8f07eb699a810e7a7fb2a5f3c289dc8
Madhur Gupta <madhurnitc@gmail.com>
2016-02-05 00:36:35 -0500

    add full test case

--- src/com/indeed/test/IndeedJobSearch.java
+++ src/com/indeed/test/IndeedJobSearch.java
@@ -1,5 +1,6 @@
 package com.indeed.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,7 +9,13 @@ public class IndeedJobSearch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new FirefoxDriver();
-		
+		driver.get("http://indeed.com");
+		driver.findElement(By.id("what")).sendKeys("Software Engineer");
+		driver.findElement(By.id("where")).clear();
+		driver.findElement(By.id("where")).sendKeys("San Jose");
+		driver.findElement(By.id("fj")).click();
+		System.out.println(driver.findElement(By.id("searchCount")).getText());
+		driver.close();
 
 	}
 
