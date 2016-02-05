13e7cbe6037aa22f617a889baffc1c892b3a2831
Madhur Gupta <madhurnitc@gmail.com>
2016-02-04 21:41:13 -0500

    add driver code

--- src/com/indeed/test/IndeedJobSearch.java
+++ src/com/indeed/test/IndeedJobSearch.java
@@ -1,9 +1,14 @@
 package com.indeed.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class IndeedJobSearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		
 
 	}
 
