563e24200ed1187368ddefeb1a5711917a051989
Madhur Gupta <madhurnitc@gmail.com>
2016-02-05 19:20:08 -0500

    add junit

---
+++ src/com/indeed/test/TestUnit.java
@@ -0,0 +1,11 @@
+package com.indeed.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class TestUnit {
+   @Test
+   public void testAdd() {
+      String str= "Junit is working fine";
+      assertEquals("Junit is working fine",str);
+   }
+}
